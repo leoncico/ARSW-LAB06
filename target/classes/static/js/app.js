ac5fe77c3b268f2402be9da302ae056ca1855b51
JeissonCasallas09 <zteban9@gmail.com>
2024-10-09 20:26:07 -0500

    Front-End Arreglado

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,24 +1,21 @@
 var App = (function () {
-    // Variables privadas
     let authorName = '';
     let blueprints = [];
 
-    // Función pública para cambiar el autor seleccionado
     function setAuthor(newAuthor) {
         authorName = newAuthor;
     }
 
-    // Función pública para actualizar el listado de planos de un autor
-    function updateBlueprintsList(author,event) {
-        // Llama a la función del módulo apimock
-        event.preventDefault();
+    function updateBlueprintsList(author) {
+ 
+        
         apimock.getBlueprintsByAuthor(author, function (blueprintsData) {
             if (!blueprintsData) {
                 alert("No blueprints found for author: " + author);
                 return;
             }
 
-            // Transformar el listado de planos
+            
             blueprints = blueprintsData.map(function (blueprint) {
                 return {
                     name: blueprint.name,
@@ -26,7 +23,7 @@ var App = (function () {
                 };
             });
 
-            // Limpiar la tabla actual
+            
             $("#tableBlueprints tbody").empty();
 
             const rows = blueprints.map(function (blueprint) {
@@ -36,20 +33,20 @@ var App = (function () {
                         </tr>`;
             });
             
-            // Unimos todas las filas generadas y las agregamos al tbody
+            
             $("#tableBlueprints tbody").append(rows.join(''));
 
-            // Calcular el total de puntos
+            
             let totalPoints = blueprints.reduce(function (sum, blueprint) {
                 return sum + blueprint.points;
             }, 0);
 
-            // Actualizar el campo del total de puntos en el DOM
+            
             $("#totalPoints").text("Total user points: " + totalPoints);
         });
     }
 
-    // Operaciones públicas
+    
     return {
         setAuthor: setAuthor,
         updateBlueprintsList: updateBlueprintsList
